36b11305f6e6dd4842ff954d2c7353cfe0e0f29b
unknown <binhu10@gmail.com>
2020-06-18 21:12:33 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
     <Router>
-    <div>
-      <Nav />
+      <div>
+        <Nav />
         <Switch>
-        <Route exact path={["/", "/books"]} >
-          <Books />
-        </Route>
-        <Route exact path="/books/:id">       
-          <Detail/>
-        </Route>  
-        <Route>
-          <NoMatch />
-        </Route>
-      </Switch>
-    </div>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,14 +4,12 @@ import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 
-function Detail() {
+function Detail(props) {
   const [book, setBook] = useState({})
-  
+
+  // When this component mounts, grab the book with the _id of props.match.params.id
+  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   const {id} = useParams()
-  // Add code to get the book with an _id equal to the id in the route param
-  // e.g. http://localhost:3000/books/:id
-  // The book id for this route can be accessed using the useParams hook
-  // from react-router-dom.
   useEffect(() => {
     API.getBook(id)
       .then(res => setBook(res.data))
